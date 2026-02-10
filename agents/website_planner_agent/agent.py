62bcdb76b30438ba08c2712203c84d00caa60ac1
Puncoz Nepal <puncoz138@gmail.com>
2026-02-10 10:14:27 +0900

    website planner agent

---
+++ agents/website_planner_agent/agent.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+from google.adk.agents import LlmAgent
+
+from utils.file_loader import load_file
+
+website_planner_agent = LlmAgent(
+    name='website_planner_agent',
+    model='gemini-2.5-flash',
+    description=load_file(filename="agents/website_planner_agent/description.txt"),
+    instruction=load_file(filename="agents/website_planner_agent/instructions.txt"),
+    output_key="website_planner_agent_output"
+)
